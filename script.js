40be391177d850ba696aba5b8ff87fb2c570dcbd
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-02-02 22:27:11 +0100

    shake and signs for player1 works

--- script.js
+++ script.js
@@ -8,41 +8,52 @@ let userPoint = 0;
 const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorsButton = document.querySelector(".scissors");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
 
 window.addEventListener("load", function () {
   start();
 
   rockButton.addEventListener("click", () => {
     // userNum = 1;
+
     userChoice = "rock";
     console.log("You chose: " + userChoice);
-    ranNumGen();
-    compChooses();
 
-    compareChoices();
-    // userChooses();
+    ranNumGen();
+    player1.classList.add("shake");
+    player1.addEventListener("animationend", compChooses);
+    player2.classList.add("shake");
+    player2.addEventListener("animationend", userChooses);
+
+    player1.classList.remove("rock", "paper", "scissors");
+    player2.classList.remove("rock", "paper", "scissors");
   });
   paperButton.addEventListener("click", () => {
     // userNum = 2;
     userChoice = "paper";
     console.log("You chose: " + userChoice);
+
     ranNumGen();
-    compChooses();
-
-    compareChoices();
-
-    // userChooses();
+    player1.classList.add("shake");
+    player1.addEventListener("animationend", compChooses);
+    player2.classList.add("shake");
+    player2.addEventListener("animationend", userChooses);
+    player1.classList.remove("rock", "paper", "scissors");
+    player2.classList.remove("rock", "paper", "scissors");
   });
   scissorsButton.addEventListener("click", () => {
     // userNum = 3;
     userChoice = "scissors";
     console.log("You chose: " + userChoice);
+
     ranNumGen();
-    compChooses();
-
-    compareChoices();
-
-    // userChooses();
+    player1.classList.add("shake");
+    player1.addEventListener("animationend", compChooses);
+    player2.classList.add("shake");
+    player2.addEventListener("animationend", userChooses);
+    player1.classList.remove("rock", "paper", "scissors");
+    player2.classList.remove("rock", "paper", "scissors");
   });
 });
 
@@ -67,9 +78,18 @@ function compChooses() {
   } else {
     compChoice = "scissors";
   }
-  //   compChoice = "paper";
 
   console.log("Computer chose; " + compChoice);
+}
+function userChooses() {
+  if (userChoice == "rock") {
+    player1.classList.add("rock");
+  } else if (userChoice == "paper") {
+    player1.classList.add("paper");
+  } else {
+    player1.classList.add("scissors");
+  }
+  compareChoices();
 }
 
 // function userChooses() {
@@ -85,8 +105,12 @@ function compChooses() {
 // }
 
 function compareChoices() {
+  player1.classList.remove("shake");
+  player2.classList.remove("shake");
+
   if (userChoice == "rock" && compChoice == "scissors") {
     userPoint++;
+
     console.log("du har: " + userPoint + "point");
   }
 
@@ -96,6 +120,7 @@ function compareChoices() {
   }
 
   if (userChoice == "scissors" && compChoice == "paper") {
+    player1.classList.add("scissors");
     userPoint++;
     console.log("du har: " + userPoint + "point");
   }
